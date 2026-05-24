1d22d4c09459b490ced15165c08f1f4f03d322c6
jul2264 <julianstevenj@gmail.com>
2026-05-24 20:03:24 +0530

    Fix Windows line endings in build script

--- backend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-python -m pip install --upgrade pip
-pip install -r requirements/production.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
